29214defdcf655ad44177b66c3b10ebc3969ad13
huangjiabiao <huangjiabiao@ly-sky.com>
2022-06-13 17:41:12 +0800

    feat:上传微信支付项目框架

---
+++ src/test/java/org/hjbwt/WxPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hjbwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
